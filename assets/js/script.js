ffe2f47aaaf8c3793486b176804b008d4b65f823
Frida010 <>
2023-10-06 12:17:51 +0000

    add

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,70 @@
 function addTask() {
-    var inputText = document.getElementById("input-text");
+    const inputElement = document.getElementById("input-text");
 
-    if (inputText === null) {
+    if (!inputElement) {
         console.error("Element with ID 'input-text' not found.");
         return;
     }
 
-    var textValue = inputText.value;
+    const textValue = inputElement.value;
 
     if (textValue === '') {
         alert("You must write something!");
     } else {
-        var listContainer = document.getElementById("list-container");
-        var listItem = document.createElement("li");
-        listItem.textContent = textValue;
-        listContainer.appendChild(listItem);
-        inputText.value = '';
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
-    }
+        const listContainer = document.getElementById("list-container");
+        const taskItem = document.createElement("li");
+        taskItem.textContent = textValue;
+        listContainer.appendChild(taskItem);
+        inputElement.value = '';
 
+        // Lägg till redigeringsfunktion
+        taskItem.addEventListener("dblclick", () => {
+            taskItem.setAttribute('contenteditable', 'true');
+            taskItem.classList.add('editing'); // Lägg till en CSS-klass
+            taskItem.focus();
+        });
+
+        taskItem.addEventListener("blur", () => {
+            taskItem.removeAttribute('contenteditable');
+            taskItem.classList.remove('editing'); // Ta bort CSS-klassen
+            saveData();
+        });
+
+        // Lägg till "Delete" -knapp
+        const deleteButton = document.createElement("span");
+        deleteButton.innerHTML = "\u00d7";
+        taskItem.appendChild(deleteButton);
+
+        // Lägg till "Delete" -knappens funktionalitet
+        deleteButton.addEventListener("click", () => {
+            taskItem.remove();
+            saveData();
+        });
+
+        saveData();
+    }
 }
 
-listContainer.addEventListenser("click", function(e) {
-    if(e.target.tagName === "LI") {
+const listContainer = document.getElementById("list-container");
+
+listContainer.addEventListener("click", (e) => {
+    if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
-    }
-    else if(e.target.tagName === "SPAN") {
-        e.target.parentElement.remove();
+        saveData();
     }
 }, false);
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
+
+function handleKeyPress(event) {
+    if (event.key === 'Enter') {
+        addTask();
+    }
+}
